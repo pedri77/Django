68a3d31883ed054900d369d3d98daaa0347af7a8
Alberto Casero <alberto@kasfactory.net>
2018-11-06 21:08:04 +0100

    Aprendemos a trabajar con el atributo GET del objeto request accediendo con seguridad a las claves del diccionario

--- ads/views.py
+++ ads/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse('Hello world!')
+    name = request.GET.get('name', 'Unknown user')
+    age = request.GET.get('age', 'unknown age')
+    return HttpResponse('Hello {0}! {0} is {1}.'.format(name, age))
