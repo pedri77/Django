4623ab23417fcaccac8a79fbb4f6b4c77b3c7ada
Alberto Casero <alberto@kasfactory.net>
2018-11-06 21:47:13 +0100

    Creamos el modelo add con sus migraciones

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Ad(models.Model):
+
+    PUBLISHED = 'PUB'
+    RESERVED = 'RES'
+    SOLD = 'SLD'
+
+    STATUS = (
+        (PUBLISHED, 'Published'),
+        (RESERVED, 'Reserved'),
+        (SOLD, 'Sold')
+    )
+
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.FloatField()
+    image = models.FileField()
+    status = models.CharField(max_length=3, choices=STATUS)
