74004ca5ca3a5ce7386139f1c31e25960ad55c14
Alberto Casero <alberto@kasfactory.net>
2018-11-07 00:07:09 +0100

    Mostramos sólo los 4 primeros anuncios

--- ads/views.py
+++ ads/views.py
@@ -5,7 +5,8 @@ from ads.models import Ad
 
 def home(request):
     # 1) Obtener los anuncios de la base de datos que están en estado Publicado
-    ads_list = Ad.objects.filter(status=Ad.PUBLISHED)
+    published_ads = Ad.objects.filter(status=Ad.PUBLISHED)
+    ads_list = published_ads[:4]
 
     # 2) Pasar los anuncios a la plantilla para que ésta los muestre en HTML
     context = {'ads': ads_list}
