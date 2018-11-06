0686c02d5260765b95367bc8ec96fe06879bc7e5
Alberto Casero <alberto@kasfactory.net>
2018-11-06 23:49:10 +0100

    Filtrando los anuncios de la home para mostrar sólo los publicados

--- ads/views.py
+++ ads/views.py
@@ -4,8 +4,8 @@ from ads.models import Ad
 
 
 def home(request):
-    # 1) Obtener los anuncios de la base de datos
-    ads_list = Ad.objects.all()
+    # 1) Obtener los anuncios de la base de datos que están en estado Publicado
+    ads_list = Ad.objects.filter(status=Ad.PUBLISHED)
 
     # 2) Pasar los anuncios a la plantilla para que ésta los muestre en HTML
     context = {'ads': ads_list}
