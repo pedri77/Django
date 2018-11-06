bbe04fae46566c170058807b6ec747bef91a96c5
Alberto Casero <alberto@kasfactory.net>
2018-11-06 22:24:56 +0100

    Haciendo que los archivos de imagen de los anuncios se guarden en una carpeta y que se sirvan en modo desarrollo

--- project/urls.py
+++ project/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -21,4 +23,4 @@ from ads.views import hello_world
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', hello_world)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
