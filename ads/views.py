f660adb47da390f3069de0597b638156ff1a099e
Alberto Casero <alberto@kasfactory.net>
2018-11-08 21:46:54 +0100

    Optimizamos las llamadas a bases de datos utilizando select_related

--- ads/views.py
+++ ads/views.py
@@ -6,7 +6,7 @@ from ads.models import Ad
 
 def home(request):
     # 1) Obtener los anuncios de la base de datos que están en estado Publicado
-    published_ads = Ad.objects.filter(status=Ad.PUBLISHED).order_by('-last_modification')
+    published_ads = Ad.objects.select_related('owner').filter(status=Ad.PUBLISHED).order_by('-last_modification')
     ads_list = published_ads[:4]
 
     # 2) Pasar los anuncios a la plantilla para que ésta los muestre en HTML
@@ -16,7 +16,7 @@ def home(request):
 
 def ad_detail(request, ad_pk):
     try:
-        ad = Ad.objects.get(pk=ad_pk)
+        ad = Ad.objects.select_related('owner').get(pk=ad_pk)
         context = {'ad': ad}
         return render(request, 'ads/ad_detail.html', context)
     except Ad.DoesNotExist:
--- project/settings.py
+++ project/settings.py
@@ -126,3 +126,26 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/uploads/'
 
+# SQL Logging
+
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
